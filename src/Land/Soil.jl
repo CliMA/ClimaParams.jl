699b7172c73c47b170ff88b95b1880995aec4b2c
gagnelandmanna <gagnelandmanna@gmail.com>
2020-06-12 17:37:29 -0400

    Removed VG, HV and BC parameters, added others

--- src/Land/Soil.jl
+++ src/Land/Soil.jl
@@ -2,50 +2,18 @@ module Soil
 
 module Water
 
-module VanGenuchten
+export Ω,
+       T1,
+       T2
 
-export n,
-       α,
-       m
+""" Impedance parameter to model reduced hydraulic conductivities in frozen soils (unitless) """
+function Ω end
 
-""" n - exponent; that then determines the exponent m used in the model () """
-function n end
+""" Parameter in expression for the temperature dependence of the hydraulic conductivity (K) """
+function T1 end
 
-""" inverse of this carries units in the expression for matric potential (inverse meters) """
-function α end
-
-""" Exponent parameter () """
-function m end
-
-end # module VanGenuchten
-
-
-module BrooksCorey
-
-export ψb, m
-
-""" ψb - units. Slightly fudged m to better match Haverkamp and VG at α=2.0 and n = 2.1. (meters) """
-function ψb end
-
-""" m - exponent """
-function m end
-
-end # module BrooksCorey
-
-module Haverkamp
-
-export k, A, B
-
-"""exponent for Yolo light clay"""
-function k end
-
-"""constant A cm^k. Our sim is in meters - convert"""
-function A end
-
-"""constant B cm^k. Our sim is in meters - convert."""
-function B end
-
-end # module Haverkamp
+""" Parameter in expression for the temperature dependence of the hydraulic conductivity (K) """
+function T2 end
 
 end # module Water
 
--- src/Land/land_parameters.jl
+++ src/Land/land_parameters.jl
@@ -1,17 +1,5 @@
+const LandWater = CLIMAParameters.Land.Soil.Water
 
-const LandVanGenuchten = CLIMAParameters.Land.Soil.Water.VanGenuchten
-
-LandVanGenuchten.n(::AbstractLandParameterSet) = 1.43
-LandVanGenuchten.α(::AbstractLandParameterSet) = 2.6
-LandVanGenuchten.m(ps::AbstractLandParameterSet) = 1-1/LandVanGenuchten.n(ps)
-
-const LandBrooksCorey = CLIMAParameters.Land.Soil.Water.BrooksCorey
-
-LandBrooksCorey.ψb(::AbstractLandParameterSet) = 0.1656
-LandBrooksCorey.m(::AbstractLandParameterSet) = 0.5
-
-const LandHaverkamp = CLIMAParameters.Land.Soil.Water.Haverkamp
-
-LandHaverkamp.k(::AbstractLandParameterSet) = 1.77
-LandHaverkamp.A(ps::AbstractLandParameterSet) = 124.6/100.0^LandHaverkamp.k(ps)
-LandHaverkamp.B(ps::AbstractLandParameterSet) = 124.6/100.0^LandHaverkamp.k(ps)
+LandWater.Ω(ps::AbstractLandParameterSet) = 7
+LandWater.T1(ps::AbstractLandParameterSet) = 507.88
+LandWater.T2(ps::AbstractLandParameterSet) = 149.3
