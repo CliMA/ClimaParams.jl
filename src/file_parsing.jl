8649afc43717839926da7840496e9ea16badde28
odunbar <odunbar@caltech.edu>
2022-02-10 23:06:02 -0800

    module names

--- src/file_parsing.jl
+++ src/file_parsing.jl
@@ -4,83 +4,6 @@ function parse_toml_file(filepath)
     return TOML.parsefile(filepath)
 end
 
-#=
-function create_alias_value_dict(full_parameter_dict)
-    alias_value_dict = Dict{String, Float64}()
-    for (key, val) in full_parameter_dict
-        # In the future - we will use the full names,
-        # param_dict[key] = val["value"]
-        
-        # for now we use the aliases
-        alias_value_dict[val["alias"]] = val["value"]
-    end
-    return alias_value_dict
-end
-
-function create_name_value_dict(full_parameter_dict)
-    name_value_dict = Dict{String, Float64}()
-    for (key, val) in full_parameter_dict
-        # In the future - we will use the full names,
-        name_value_dict[key] = val["value"]
-    end
-    return name_value_dict
-end
-
-function merge_override_default_values(override_param_dict,default_param_dict)
-    merged_dict = default_param_dict
-    for (key, val) in override_param_dict
-        merged_dict[key] = val
-    end
-    return merged_dict
-end
-
-function create_param_dict(full_parameter_dict, dict_type)
-    if dict_type == "alias"
-        return create_alias_value_dict(full_parameter_dict)
-    elseif dict_type == "name"
-        return create_name_value_dict(full_parameter_dict)
-    else
-        throw(ArgumentError("Unknown dict_type, choose from \"alias\" or \"name\""))
-    end
-end
-
-function create_parameter_dict(path_to_override, path_to_default; dict_type="alias")
-    #if there isn't  an override file take defaults
-    if isnothing(path_to_override)
-        return create_param_dict(parse_toml_file(path_to_default), dict_type)
-    else
-        try 
-            override_param_dict = create_param_dict(parse_toml_file(path_to_override), dict_type)
-            default_param_dict = create_param_dict(parse_toml_file(path_to_default), dict_type)
-        
-            #overrides the defaults where they clash
-            return merge_override_default_values(override_param_dict, default_param_dict)
-        catch
-            @warn("Error in building from parameter file: ", path_to_override,"instead, created using defaults from CLIMAParameters...")
-            return create_param_dict(parse_toml_file(path_to_default), dict_type)
-        end
-    end
-        
-end
-
-function create_parameter_dict(path_to_override; dict_type="alias")
-    #pathof finds the CLIMAParameters.jl/src/ClimaParameters.jl location
-    path_to_default = joinpath(splitpath(pathof(CLIMAParameters))[1:end-1]...,"parameters.toml")
-    return create_parameter_dict(
-        path_to_override,
-        path_to_default,
-        dict_type=dict_type
-    )
-end
-
-function create_parameter_dict(; dict_type="alias")
-    return create_parameter_dict(
-        nothing,
-        dict_type=dict_type
-    )
-end
-=#
-# Alternatively do it with a wrapper struct. 
 struct ParamDict{FT}
     data::Dict
     dict_type::String
@@ -128,14 +51,15 @@ end
 log_component!(param_set::ParamDict{FT},names,component) where {FT} = log_component!(param_set.data,names,component,param_set.dict_type)
 
 function log_component!(data::Dict,names,component,dict_type)
+    component_key = "used_in"
     if dict_type == "alias"
         for name in names
             for (key,val) in data
                 if name == val["alias"]
-                    if ["used_in_components"] in keys(data[key])
-                        append!(data[key]["used_in_components"],[String(Symbol(component))])
+                    if component_key in keys(data[key])
+                        data[key][component_key] = unique([data[key][component_key]...,String(Symbol(component))])
                     else
-                        data[key]["used_in_components"] = [String(Symbol(component))]
+                        data[key][component_key] = [String(Symbol(component))]
                     end
                 end
             end
@@ -144,10 +68,10 @@ function log_component!(data::Dict,names,component,dict_type)
          for name in names
             for (key,val) in data
                 if name == key
-                    if ["used_in_components"] in keys(data[key])
-                        append!(data[key]["used_in_components"],[String(Symbol(component))])
+                    if component_key in keys(data[key])
+                        data[key][component_key] = unique([data[key][component_key]...,String(Symbol(component))])
                     else
-                        data[key]["used_in_components"] = [String(Symbol(component))]
+                        data[key][component_key] = [String(Symbol(component))]
                     end
                 end
             end
