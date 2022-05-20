55e2cbac1fe6209b5c8c90c1e9f09292e734e8d4
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-05-20 14:29:04 -0700

    Remove method-based support
    Update tests to not depend on old CLIMAParameters methods

--- src/file_parsing.jl
+++ src/file_parsing.jl
@@ -1,17 +1,3 @@
-using TOML
-using DocStringExtensions
-
-
-export AbstractTOMLDict
-export ParamDict, AliasParamDict
-
-export float_type,
-    get_parameter_values!,
-    get_parameter_values,
-    write_log_file,
-    log_parameter_information,
-    create_toml_dict
-
 """
     AbstractTOMLDict{FT <: AbstractFloat}
 
--- test/old/subgrid_scale.jl
+++
@@ -1,9 +0,0 @@
-using Test
-using CLIMAParameters: AbstractEarthParameterSet
-using CLIMAParameters.SubgridScale
-
-@testset "SubgridScale" begin
-    struct EarthParameterSet <: AbstractEarthParameterSet end
-    ps = EarthParameterSet()
-    @test von_karman_const(ps) ≈ 0.4
-end
